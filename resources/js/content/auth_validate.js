d15d227f0b27edd083cd85be6cbb1233f06749d1
f0l13N <97778517+f0l13N@users.noreply.github.com>
2022-06-07 12:33:09 +0500

    Revert "fix error"
    This reverts commit f7d431be6299e0dc0c17ae13cbab9570013415c4.

--- resources/js/content/auth_validate.js
+++ resources/js/content/auth_validate.js
@@ -2,46 +2,53 @@
  * валидатор для авторизации и регистрации
  */
 
-export function auth_validate() {
-  const phone = document.querySelector('#phone').value;
-  const password = document.querySelector('#password').value;
+export function auth_validate()
+{
+	let phone = document.getElementById('phone').value;
+    let password = document.getElementById('password').value;
 
-  const resultPhone = validatePhone(phone);
-  const resultPassword = validatePassword(password);
+    let resultPhone = validatePhone(phone);
+    let resultPassword = validatePassword(password);
 
-  if (resultPhone && resultPassword) return true;
+    if (resultPhone && resultPassword) return true;
 
-  return false;
+    return false;
 }
 
-function validatePhone(phone) {
-  if (phone === '') {
-    printError('phoneErrDiv', 'Телефон не введен!');
-    return false;
-  }
-  const regex = /\d/; // здесь указывать условия отбора
-  if (regex.test(phone) === false) {
-    printError('phoneErrDiv', 'Телефон не подходит!');
-    return false;
-  }
-  printError('phoneErrDiv', '');
-  return true;
+function validatePhone(phone)
+{
+	if(phone == "") {
+		printError("phoneErrDiv", "Телефон не введен!");
+        return false;
+    } else {
+        var regex = /[0-9]/;   //здесь указывать условия отбора
+        if(regex.test(phone) === false) {
+        	printError("phoneErrDiv", "Телефон не подходит!");
+            return false;
+        } else{
+        	printError("phoneErrDiv", "");
+            return true;
+        }
+    }
 }
 
-function validatePassword(password) {
-  if (password === '') {
-    printError('passwordErrDiv', 'Пароль не введен!');
-    return false;
-  }
-  const regex = /[a-z]/; // здесь указывать условия отбора
-  if (regex.test(password) === false) {
-    printError('passwordErrDiv', 'Пароль не подходит!');
-    return false;
-  }
-  printError('passwordErrDiv', '');
-  return true;
+function validatePassword(password)
+{
+	if(password == "") {
+        printError("passwordErrDiv", "Пароль не введен!");
+        return false;
+    } else {
+        var regex = /[a-z]/;   //здесь указывать условия отбора
+        if(regex.test(password) === false) {
+            printError("passwordErrDiv", "Пароль не подходит!");
+            return false;
+        } else{
+        	printError("passwordErrDiv", "");
+            return true;
+        }
+    }
 }
 
 function printError(divId, text) {
-  document.getElementById(divId).innerHTML = text;
+    document.getElementById(divId).innerHTML = text;
 }
